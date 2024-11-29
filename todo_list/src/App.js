45d80a16436192120d3fff05161d0db4ad7b21f1
Viktoria <vinem305@mail.ru>
2024-11-29 14:50:23 +0700

    New folder with a project

---
+++ todo_list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style/style.css'
+import { List } from './components/list';
+
+
+function App() {
+  return (
+    <div>
+      <List />
+    </div>
+  )
+}
+
+export default App;
